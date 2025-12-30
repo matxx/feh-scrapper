26a16d85d96278905aa0a5466498cb330dd0d8a0
matxx <matxx.webmaster@gmail.com>
2025-12-31 00:52:56 +0100

    refactor: move INT_ID* constants into module

--- lib/scrappers/fandoms/units.rb
+++ lib/scrappers/fandoms/units.rb
@@ -10,6 +10,7 @@ require 'active_support/core_ext/string/conversions'
 require 'scrappers/fandoms/units/abbreviated_names'
 require 'scrappers/fandoms/units/scores'
 require 'scrappers/fandoms/units/themes'
+require 'scrappers/fandoms/units/unit_ids'
 
 module Scrappers
   module Fandoms
@@ -17,6 +18,7 @@ module Scrappers
       include Scrappers::Fandoms::Units::AbbreviatedNames
       include Scrappers::Fandoms::Units::Scores
       include Scrappers::Fandoms::Units::Themes
+      include Scrappers::Fandoms::Units::UnitIds
 
       attr_reader(
         :all_units,
@@ -169,7 +171,7 @@ module Scrappers
           unit[:properties] = (unit['Properties'] || '').split(',')
 
           if unit[:properties].intersect?(FIVE_STAR_FOCUS_ONLY_UNIT_PROPERTIES) ||
-             INT_IDS_OF_FOCUS_ONLY_UNITS.include?(unit[:int_id])
+             self.class::INT_IDS_OF_FOCUS_ONLY_UNITS.include?(unit[:int_id])
             unit[:is_in][:focus_only] = true
             unit[:lowest_rarity][:focus_only] = 5
           elsif unit[:properties].include?('ghb')
@@ -280,40 +282,6 @@ module Scrappers
 
         nil
       end
-
-      INT_ID_ANNIVERSARY_MARTH = 1235
-      INT_ID_D_ELINCIA = 1303
-      INT_ID_H_B_IKE = 1315
-      INT_ID_H_B_LYN = 1316
-      INT_IDS_OF_FOCUS_ONLY_UNITS = [
-        INT_ID_ANNIVERSARY_MARTH,
-        INT_ID_D_ELINCIA,
-        INT_ID_H_B_IKE,
-        INT_ID_H_B_LYN,
-      ].freeze
-
-      INT_ID_NY_CORRIN = 200
-
-      INT_ID_MARISA = 212
-      INT_ID_GEROME = 228
-      INT_ID_FINN = 238
-      INT_ID_CANAS = 258
-      INT_ID_ETHLYN = 288
-      INT_ID_AZURA_YOUNG = 312
-      INT_ID_SIGRUN = 415
-      INT_ID_BRUNNYA = 452
-      INT_ID_ITSUKI = 480
-      INT_IDS_OF_TT_UNITS_WITHOUT_THEME = [
-        INT_ID_MARISA,
-        INT_ID_GEROME,
-        INT_ID_FINN,
-        INT_ID_CANAS,
-        INT_ID_ETHLYN,
-        INT_ID_AZURA_YOUNG,
-        INT_ID_SIGRUN,
-        INT_ID_BRUNNYA,
-        INT_ID_ITSUKI,
-      ].freeze
 
       def export_units
         export_files(
@@ -526,60 +494,6 @@ module Scrappers
           ),
         )
       end
-
-      INT_ID_CAMILLA_ADRIFT = 308
-      INT_ID_CORRIN_M_ADRIFT = 309
-      INT_ID_CORRIN_F_ADRIFT = 310
-
-      INT_ID_EIRIKA_TOME = 209
-      INT_ID_CHROM_CAV = 225
-      INT_ID_REINHARDT_SWORD = 234
-      INT_ID_OLWEN_GREEN = 235
-      INT_ID_HINOKA_BOW = 240
-      INT_ID_NINO_FLYING = 255
-      INT_ID_OLIVIA_FLYING = 269
-
-      INT_ID_MARTH_FE13 = 136
-      INT_ID_ANNA_FE13 = 527
-      INT_ID_SELENA_FE8 = 596
-      # INT_ID_HILDA_FE16 = 409
-      INT_ID_HILDA_FE4 = 797
-      # INT_ID_ARTHUR_FE14 = 52
-      INT_ID_ARTHUR_FE4 = 795
-
-      INT_ID_CATRIA_SOV = 434
-      INT_ID_PALLA_SOV = 667
-      INT_ID_EST_SOV = 829
-
-      INT_ID_TIKI_A = 95
-      INT_ID_TIKI_A_SU = 146
-      INT_ID_TIKI_A_B = 841
-      INT_ID_TIKI_A_BR = 950
-      INT_IDS_OF_ADULTS = [
-        INT_ID_TIKI_A,
-        INT_ID_TIKI_A_SU,
-        INT_ID_TIKI_A_B,
-        INT_ID_TIKI_A_BR,
-      ].freeze
-
-      INT_ID_AZURA_Y = 312
-      INT_ID_TIKI_Y = 94
-      INT_ID_TIKI_Y_SU = 267
-      INT_ID_TIKI_Y_L = 297
-      INT_ID_TIKI_Y_H = 571
-      INT_ID_TIKI_Y_AS = 874
-      INT_ID_TIKI_Y_AT = 1230
-      INT_ID_TIKI_Y_F = 372
-      INT_IDS_OF_YOUNGS = [
-        INT_ID_AZURA_Y,
-        INT_ID_TIKI_Y,
-        INT_ID_TIKI_Y_SU,
-        INT_ID_TIKI_Y_L,
-        INT_ID_TIKI_Y_H,
-        INT_ID_TIKI_Y_AS,
-        INT_ID_TIKI_Y_AT,
-        INT_ID_TIKI_Y_F,
-      ].freeze
 
       # B!Eirika & P!Hinoka have same version 5.8 but different max DF
       # => diff between release dates (and not versions)
---
+++ lib/scrappers/fandoms/units/unit_ids.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+module Scrappers
+  module Fandoms
+    module Units
+      module UnitIds
+        INT_ID_ANNIVERSARY_MARTH = 1235
+        INT_ID_D_ELINCIA = 1303
+        INT_ID_H_B_IKE = 1315
+        INT_ID_H_B_LYN = 1316
+        INT_IDS_OF_FOCUS_ONLY_UNITS = [
+          INT_ID_ANNIVERSARY_MARTH,
+          INT_ID_D_ELINCIA,
+          INT_ID_H_B_IKE,
+          INT_ID_H_B_LYN,
+        ].freeze
+
+        INT_ID_NY_CORRIN = 200
+
+        INT_ID_MARISA = 212
+        INT_ID_GEROME = 228
+        INT_ID_FINN = 238
+        INT_ID_CANAS = 258
+        INT_ID_ETHLYN = 288
+        INT_ID_AZURA_YOUNG = 312
+        INT_ID_SIGRUN = 415
+        INT_ID_BRUNNYA = 452
+        INT_ID_ITSUKI = 480
+        INT_IDS_OF_TT_UNITS_WITHOUT_THEME = [
+          INT_ID_MARISA,
+          INT_ID_GEROME,
+          INT_ID_FINN,
+          INT_ID_CANAS,
+          INT_ID_ETHLYN,
+          INT_ID_AZURA_YOUNG,
+          INT_ID_SIGRUN,
+          INT_ID_BRUNNYA,
+          INT_ID_ITSUKI,
+        ].freeze
+
+        INT_ID_CAMILLA_ADRIFT = 308
+        INT_ID_CORRIN_M_ADRIFT = 309
+        INT_ID_CORRIN_F_ADRIFT = 310
+
+        INT_ID_EIRIKA_TOME = 209
+        INT_ID_CHROM_CAV = 225
+        INT_ID_REINHARDT_SWORD = 234
+        INT_ID_OLWEN_GREEN = 235
+        INT_ID_HINOKA_BOW = 240
+        INT_ID_NINO_FLYING = 255
+        INT_ID_OLIVIA_FLYING = 269
+
+        INT_ID_MARTH_FE13 = 136
+        INT_ID_ANNA_FE13 = 527
+        INT_ID_SELENA_FE8 = 596
+        # INT_ID_HILDA_FE16 = 409
+        INT_ID_HILDA_FE4 = 797
+        # INT_ID_ARTHUR_FE14 = 52
+        INT_ID_ARTHUR_FE4 = 795
+
+        INT_ID_CATRIA_SOV = 434
+        INT_ID_PALLA_SOV = 667
+        INT_ID_EST_SOV = 829
+
+        INT_ID_TIKI_A = 95
+        INT_ID_TIKI_A_SU = 146
+        INT_ID_TIKI_A_B = 841
+        INT_ID_TIKI_A_BR = 950
+        INT_IDS_OF_ADULTS = [
+          INT_ID_TIKI_A,
+          INT_ID_TIKI_A_SU,
+          INT_ID_TIKI_A_B,
+          INT_ID_TIKI_A_BR,
+        ].freeze
+
+        INT_ID_AZURA_Y = 312
+        INT_ID_TIKI_Y = 94
+        INT_ID_TIKI_Y_SU = 267
+        INT_ID_TIKI_Y_L = 297
+        INT_ID_TIKI_Y_H = 571
+        INT_ID_TIKI_Y_AS = 874
+        INT_ID_TIKI_Y_AT = 1230
+        INT_ID_TIKI_Y_F = 372
+        INT_IDS_OF_YOUNGS = [
+          INT_ID_AZURA_Y,
+          INT_ID_TIKI_Y,
+          INT_ID_TIKI_Y_SU,
+          INT_ID_TIKI_Y_L,
+          INT_ID_TIKI_Y_H,
+          INT_ID_TIKI_Y_AS,
+          INT_ID_TIKI_Y_AT,
+          INT_ID_TIKI_Y_F,
+        ].freeze
+      end
+    end
+  end
+end
