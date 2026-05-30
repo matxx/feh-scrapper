6637129919d8bf291e591f34b90d086478bbcc19
matxx <matxx.webmaster@gmail.com>
2026-05-30 07:49:17 +0200

    chore: 2026-05-30 update

--- lib/scrappers/s3.rb
+++ lib/scrappers/s3.rb
@@ -6,7 +6,7 @@ module Scrappers
   class S3 < Base
     attr_reader :now, :logger, :all_seals_by_id, :all_skills_by_id
 
-    COMMIT = '9a72a9fcfc5c158383403a68fb2a8b9806a8f60e'
+    COMMIT = '37de17e05542e4bf9a5e23e043dd015ff7d0b68a'
     DIR = "https://data.feh-peeler.com/commits/#{COMMIT}".freeze
 
     def initialize(level: Logger::ERROR)
